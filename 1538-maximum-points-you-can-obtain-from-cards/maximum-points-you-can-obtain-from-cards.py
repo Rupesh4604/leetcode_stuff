cc856ef92dd8178541c069c5b40f3c0f29122104
Rupesh <rupesh32003@gmail.com>
2024-05-02 15:01:33 +0530

    Time: 304 ms (85.51%) | Memory: 22 MB (25.36%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
@@ -5,31 +5,14 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        # # leftbound = k-1     # starting to K size window 
-        # # rightbound = len(cardPoints)-1
-        # n = len(cardPoints)
-        # maxi = 0
-        # # while (leftbound>=0):
-        # #     maxi = max(sum(cardPoints[:leftbound]+cardPoints[rightbound:]),maxi)
-        # #     leftbound -=1
-        # #     rightbound -=1
-        # for i in range(k):
-        #     checksum = sum(cardPoints[:k-i-1]+cardPoints[n-1-i:])
-        #     maxi = max(checksum,maxi)
-        # return maxi
+        points = sum(cardPoints[:k])
         n = len(cardPoints)
-        window_size = n - k
-        total_sum = sum(cardPoints)
-        min_subarray_sum = float('inf')
-        
-        if window_size == 0:
-            return total_sum
-        
-        current_sum = sum(cardPoints[:window_size])
-        min_subarray_sum = min(min_subarray_sum, current_sum)
-        
-        for i in range(window_size, n):
-            current_sum += cardPoints[i] - cardPoints[i - window_size]
-            min_subarray_sum = min(min_subarray_sum, current_sum)
-        
-        return total_sum - min_subarray_sum
+        points_temp = points
+		
+        for i in range(k):
+            left_idx_to_substract = k - i - 1
+            right_idx_to_add = n - i - 1
+            points_temp -= cardPoints[left_idx_to_substract]
+            points_temp += cardPoints[right_idx_to_add]
+            points = max(points, points_temp)
+        return points
