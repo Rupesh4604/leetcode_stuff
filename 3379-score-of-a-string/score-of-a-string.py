d3a36cf0fea2849f02d9a7cad51b42b58153efb7
Rupesh <rupesh32003@gmail.com>
2024-06-10 11:43:51 +0530

    Time: 43 ms (13.85%) | Memory: 16.7 MB (18.22%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.py
+++ 3379-score-of-a-string/score-of-a-string.py
@@ -1,6 +1,6 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
         res = 0
-        for i in range(1,len(s)):
-            res += abs(ord(s[i])-ord(s[i-1]))
+        for i in range(len(s)-1):
+            res += abs(ord(s[i+1])-ord(s[i]))
         return res
