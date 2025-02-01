da58b0a6073ea52f6241a8525dd5a1af11a22d65
Rupesh <rupesh32003@gmail.com>
2025-02-02 00:06:20 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (10.12%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,3 +1,24 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return str(bin(int(a,2)+int(b,2)))[2:]
+        '''
+        A simple answer using int function int(a,2) where 2 represents the base
+        str(bin(int(a,2)+int(b,2)))[2:]
+        '''
+        carry = 0
+        max_len = max(len(a), len(b))
+        a = a.zfill(max_len)
+        b = b.zfill(max_len)
+
+        res = ''
+        for i in range(max_len-1,-1,-1):
+            n1 = int(a[i])
+            n2 = int(b[i])
+
+            sum_digits = carry + n1 + n2
+            carry = sum_digits//2
+            res = str(sum_digits % 2) + res
+        
+        if carry:
+            res = "1"+ res
+        
+        return res
