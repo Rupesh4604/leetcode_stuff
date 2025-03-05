70fc0f9103b806fa116271d08044a36b22312a84
Rupesh <rupesh32003@gmail.com>
2025-03-05 23:04:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (75.19%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.py
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.py
@@ -1,8 +1,9 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        if n==1:
-            return 1
-        total = 1
-        for i in range(1,n):
-            total += 4*i
-        return total
+        # if n==1:
+        #     return 1
+        # total = 1
+        # for i in range(1,n):
+        #     total += 4*i
+        # return total
+        return 1+2*n*(n-1)
