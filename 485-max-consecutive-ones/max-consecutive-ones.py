4d94f3a3de2eb36cf1062cf3c391cd37271f6e1b
Rupesh <rupesh32003@gmail.com>
2025-09-15 03:59:31 +0530

    Time: 7 ms (96.54%) | Memory: 20 MB (98.48%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.py
+++ 485-max-consecutive-ones/max-consecutive-ones.py
@@ -6,7 +6,9 @@ class Solution:
             if elem==1:
                 count +=1
             else:
-                res = max(res, count)
+                res = max(res, count) # runs only when case is true
                 count = 0
-        res = max(res, count)    
+            # res = max(res, count) # this runs O(n) times
+        res = max(res, count)  # Runs only once to handle edge cases
+        # example where the max ones are in the end with no break  
         return res 
