f5d3d1201383ff8c062893208c9780cd1bf17e58
Rupesh <rupesh32003@gmail.com>
2025-09-15 23:59:09 +0530

    Time: 6 ms (16.27%) | Memory: 19.1 MB (8.11%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -3,7 +3,7 @@ class Solution:
         # given nums are sorted, if not sorted use a hashmap/set
         count = 1  
         for i in range(1,len(nums)):
-            if (nums[i]-nums[i-1])>0:
+            if nums[i]!=nums[i-1]:
                 nums[count] = nums[i]
                 count+=1
         return count
