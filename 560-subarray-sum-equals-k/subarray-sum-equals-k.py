e384159dc4fb8151c432709324dde46bb76f153b
Rupesh <rupesh32003@gmail.com>
2025-11-01 17:43:48 +0530

    Time: 41 ms (24.86%) | Memory: 22.1 MB (5.50%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,13 +1,12 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        preSum = 0
-        ds = defaultdict(int)
+        ds = defaultdict(int) # hashmap of prefixsum:count
+        prefixSum = 0
         count = 0
-        ds[0]=1
+        ds[0] = 1   # prefixsum=0 -> count=1
         for i in range(len(nums)):
-            preSum += nums[i]
-            diff = preSum - k
-            count += ds[diff]
-            ds[preSum] = ds.get(preSum,0)+1
-        return count
-
+            prefixSum += nums[i]
+            diff =  prefixSum - k
+            count += ds[diff] 
+            ds[prefixSum] = ds.get(prefixSum,0) + 1 
+        return count 
