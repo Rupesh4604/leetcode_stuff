3231d00b2508af0b0d3d2ed942762fb9c0155677
Rupesh <rupesh32003@gmail.com>
2025-11-11 16:05:50 +0530

    Time: 1 ms (65.53%) | Memory: 17.6 MB (98.03%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,5 +1,6 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         counts = Counter(words)
+        # Sort by: frequency ↓ (negative for descending), then lexicographically ↑
         res = sorted(counts,key=lambda x: (-counts[x], x))[:k]
         return res
