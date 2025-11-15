0b536316277744058295caedba75a8d5c441a542
Rupesh <rupesh32003@gmail.com>
2025-11-15 15:38:55 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (69.84%) - LeetSync

---
+++ 733-flood-fill/flood-fill.py
@@ -0,0 +1,22 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        m = len(image)
+        n = len(image[0])
+        preCol = image[sr][sc]
+        if preCol==color:
+            return image
+
+        q = deque([(sr,sc)])
+
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+
+        while q:
+            r,c = q.popleft()
+            for dx,dy in directions:
+                x,y = r+dx,c+dy
+                if 0<=x<m and 0<=y<n and image[x][y]==preCol:
+                    image[x][y]=color
+                    q.append((x,y))
+        image[sr][sc]=color
+        return image
+                
